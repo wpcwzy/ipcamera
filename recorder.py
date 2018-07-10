700780ff0c9c4d09e13c114e85a9fafd0649a5af
wpcwzy <wpc.369623245@live.cn>
2018-07-10 19:19:23 +0800

    Fixed PTZ control module Add cycle record feature

--- recorder.py
+++ recorder.py
@@ -2,20 +2,16 @@ import cv2
 import time
 import requests
 
-videoStartTime = 0
+nowTime = time.time()
+videoStartTime = nowTime
+videoMaxNum = 64
+nowTime = time.time()
+videoNum = 0
+videoTime = 600
 
-output = cv2.VideoWriter('output.avi', 4, 8, (640, 480))
+output = cv2.VideoWriter(str(videoNum) + '.avi', 4, 4, (640, 480))
 video = cv2.VideoCapture('http://192.168.0.126:81/videostream.cgi?user=raspberry&pwd=pi')
 print "Is video Opened:"+str(video.isOpened())
-
-def record():
-    isSuccess, frame = video.read()
-    cv2.imshow('frame', frame)
-    output.write(frame)
-    cleanup()
-
-def cleanup():
-    a = 1;
 
 
 def PTZ(model):
@@ -33,24 +29,35 @@ def PTZ(model):
 
 
 if __name__=='__main__':
-    while (video.isOpened()):
-        record()
+    while (video.isOpened() & output.isOpened()):
+        isSuccess, frame = video.read()
+        cv2.imshow('frame', frame)
+        output.write(frame)
         nowTime = time.time()
-        if cv2.waitKey(100) == ord('q'):
+        if nowTime - videoStartTime >= videoTime:
+            output.release
+            temp = videoNum + 1
+            videoNum = temp
+            if videoNum >= videoMaxNum:
+                videoNum = 0
+            output = cv2.VideoWriter(str(videoNum) + '.avi', 4, 7, (640, 480))
+            videoStartTime = nowTime
+        pressedKey = cv2.waitKey(1)
+        if pressedKey == 113:  # q
             break
-        if cv2.waitKey(100) == ord('w'):
+        if pressedKey == 119:  #w
             print "PTZ up"
             PTZ(1)
-        if cv2.waitKey(100) == ord('a'):
+        if pressedKey == 97:  #a
             print "PTZ left"
             PTZ(2)
-        if cv2.waitKey(100) == ord('s'):
+        if pressedKey == 115:  #s
             print "PTZ down"
             PTZ(3)
-        if cv2.waitKey(100) == ord('d'):
+        if pressedKey == 100:  #d
             print "PTZ right"
             PTZ(4)
-        if cv2.waitKey(100) == ord('z'):
+        if pressedKey == 122:  #z
             print "PTZ stop"
             PTZ(5)
 
