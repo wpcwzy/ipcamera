cb32fd215380c8efeb48c1e33c81d6bbf2eb33d8
wpcwzy <wpc.369623245@live.cn>
2018-08-19 18:36:21 +0800

    Add Adaptive fps moudule

--- recorder.py
+++ recorder.py
@@ -11,17 +11,23 @@ videoMaxNum = 150  # around 15GB,25hours
 nowTime = time.time()
 videoNum = 0
 videoTime = 600
+fps = 0
+framecount = 0
 
 retryTime = 0
 waitTime = 5
-
 cameraName = "Camera 1"
 
-output = cv2.VideoWriter(str(videoNum) + '.avi', 4, 4, (640, 480))
+
 video = cv2.VideoCapture('http://192.168.0.126:81/videostream.cgi?user=raspberry&pwd=pi')
 logFile = "log.log"
 print "Is video Opened:" + str(video.isOpened())
 
+frameStartTime = time.time()
+while (time.time() - frameStartTime <= 1):
+    isSuccess, frame = video.read()
+    fps = fps + 1
+output = cv2.VideoWriter(str(videoNum) + '.avi', 4, fps, (640, 480))
 
 def logger(string):
     log = open(logFile, "a")
@@ -46,12 +52,13 @@ def PTZ(model):
         requests.post("http://192.168.0.126:81/decoder_control.cgi?user=raspberry&pwd=pi&command=1")
         logger("PTZ stop\n")
 
-
 if __name__ == '__main__':
     try:
         while (not video.isOpened()):
             temp = retryTime + 1
             retryTime = temp
+            output = cv2.VideoWriter(str(videoNum) + '.avi', 4, fps, (640, 480))
+            video = cv2.VideoCapture('http://192.168.0.126:81/videostream.cgi?user=raspberry&pwd=pi')
             print("Camera Open Failed.Retry " + str(retryTime) + " Times")
             print("Sleep " + str(waitTime) + " seconds")
             logger("Camera Open Failed.Retry " + str(retryTime) + " Times\n")
@@ -74,7 +81,12 @@ if __name__ == '__main__':
                 videoNum = temp
                 if videoNum >= videoMaxNum:
                     videoNum = 0
-                output = cv2.VideoWriter(str(videoNum) + '.avi', 4, 7, (640, 480))
+                fps = 0
+                frameStartTime = time.time()
+                while (time.time() - frameStartTime <= 1):
+                    isSuccess, frame = video.read()
+                    fps = fps + 1
+                output = cv2.VideoWriter(str(videoNum) + '.avi', 4, fps, (640, 480))
                 logger("Now Recording:" + str(videoNum) + ".avi\n")
                 videoStartTime = nowTime
             pressedKey = cv2.waitKey(1)
